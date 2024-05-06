1184996d950112e9580f9def6523164ce28d3101
Lucas Eiji <104649020+eijilucas@users.noreply.github.com>
2024-05-06 14:25:50 -0300

    feat: product card

---
+++ src/components/ProductCard/product-card.jsx
@@ -0,0 +1,24 @@
+import "./product-card.scss"
+import { FaShoppingCart } from "react-icons/fa";
+
+export default function ProductCard({data}) {
+
+    const { thumbnail, title, price } = data;
+
+    return (
+        <section className="product_card">
+            <img src={thumbnail} alt="" className="product_card_img"/>
+
+            <div className="product_card_infos">
+                <h2 className="product_card_title">{title}</h2>
+                <h2 className="product_card_price">{price}</h2>
+            </div>
+
+            <button className="add_cart_btn">
+                <FaShoppingCart/>
+                <span>Add to cart</span>
+            </button>
+
+        </section>
+    )
+}
