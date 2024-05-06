ab3ae29ebe692ba1cbdabfbf8d7c5280006af75f
Lucas Eiji <104649020+eijilucas@users.noreply.github.com>
2024-05-06 18:34:22 -0300

    feat: search bar

--- src/components/SearchBar/search-bar.jsx
+++ src/components/SearchBar/search-bar.jsx
@@ -1,14 +1,19 @@
 import "./search-bar.scss";
 import { FaSearch } from "react-icons/fa";
-import { useState } from "react";
-
+import { useContext, useState } from "react";
+import { AppContext } from '../../contexts/AppContext';
+import fetchProducts from '../../api/FetchProducts';
 
 export default function SearchBar() {
 
     const [searchValue, setSearchValue] = useState("");
+    const { setProducts } = useContext(AppContext);
 
-    const handleSearch = () => {
-
+    const handleSearch = async (e) => {
+        e.preventDefault();
+        const products = await fetchProducts(searchValue);
+        setProducts(products)
+        setSearchValue("")
     }
 
   return (
