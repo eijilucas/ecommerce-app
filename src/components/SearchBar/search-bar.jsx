9d58f38e0938da22739cb73dd247ddf12cf0994d
Lucas Eiji <104649020+eijilucas@users.noreply.github.com>
2024-05-06 11:10:21 -0300

    fix

--- src/components/SearchBar/search-bar.jsx
+++ src/components/SearchBar/search-bar.jsx
@@ -1,28 +1,29 @@
 import "./search-bar.scss";
 import { FaSearch } from "react-icons/fa";
-import { useContext } from "react";
+import { useState } from "react";
+
 
 export default function SearchBar() {
 
-    const {searchValue, setSearchValue} = useContext();
+    const [searchValue, setSearchValue] = useState("");
 
     const handleSearch = () => {
-        
+
     }
 
   return (
-    <section className="search">
+    <form className="search" onSubmit={handleSearch}>
       <input 
       type="text" 
       placeholder="What can we help you find today?" 
       value={searchValue}
-      onChange={(e) => [setSearchValue(e.target.value)]}
+      onChange={(e) => setSearchValue(e.target.value)}
       />
 
-      <button type="submit" onClick={handleSearch}>
+      <button type="submit">
         <FaSearch size={20} />
       </button>
 
-    </section>
+    </form>
   );
 }
